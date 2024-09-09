48ec78f0f31f6004ff55a343fd5dabc7b79e867b
Roxane Braconnier <roxane.braconnier@efrei.net>
2024-09-09 17:38:06 +0200

    Camera movements

--- Assets/Script/Camera_behav.cs
+++ Assets/Script/Camera_behav.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,20 +9,52 @@ public class Camera_behav : MonoBehaviour
     public Camera cam;
     private float min = 55f;
     private float max = 95f;
-    private float scrollSpeed = 10f; // Vitesse de changement du FOV
+    private float scrollSpeed = 10f; 
+
+    private Vector2 turn;
 
     void Start()
     {
         cam = GetComponent<Camera>();
-        cam.fieldOfView = max; // Initialiser avec la valeur max
+        cam.fieldOfView = max; 
     }
 
     void Update()
     {
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-
         cam.fieldOfView -= scroll * scrollSpeed;
-
         cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, min, max);
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            movingCamera_wt_player();
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            movingCamera_w_player();
+        }
+
+ 
+    }
+
+
+
+    private void movingCamera_wt_player(){
+
+        turn.x = Input.GetAxis("Mouse X") * Time.deltaTime * 10f; 
+        turn.y = Input.GetAxis("Mouse Y") * Time.deltaTime * 10f; 
+        
+
+        transform.RotateAround(player.transform.position, Vector3.up, turn.x );
+        transform.RotateAround(player.transform.position, transform.right, -turn.y );
+    
+    }
+
+    private void movingCamera_w_player(){
+        turn.x = Input.GetAxis("Mouse X") * Time.deltaTime * 10f; 
+
+        player.transform.Rotate(Vector3.up * turn.x, Space.World);
+
+
     }
 }
